1a3ba793af46bbb7b74ea5b3ae37eb3f0516f18c
Dayton <daytonj85@gmail.com>
2025-04-20 01:43:06 -0400

    config correction (DB Naming)

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,7 +9,7 @@ from pydantic import (
 class Settings(BaseSettings):
     
     PROJECT_NAME: str
-    POSTGRES_NAME: str
+    POSTGRES_DB: str
     PROJECT_VERSION: str
     POSTGRES_SERVER: str
     POSTGRES_PORT: int
@@ -28,4 +28,3 @@ class Settings(BaseSettings):
 settings = Settings()
 
 print(f"Project Name: {settings.PROJECT_NAME}")
-print(f"Project Version: {settings.POSTGRES_SERVER}")  
